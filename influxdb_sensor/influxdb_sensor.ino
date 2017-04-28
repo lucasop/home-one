8dd4580c8c9843b84ea900e1c6cc033639fbf0c6
lucasop <lucasop@gmail.com>
2017-04-28 21:11:42 +0200

    sensore RealPower
    sensore RealPower

---
+++ influxdb_sensor/influxdb_sensor.ino
@@ -0,0 +1,362 @@
+#include "Arduino.h"
+/*
+ *  This sketch sends ads1115 current sensor data via HTTP POST request to thingspeak server.
+ *  It needs the following libraries to work (besides the esp8266 standard libraries supplied with the IDE):
+ *
+ *  - https://github.com/adafruit/Adafruit_ADS1X15
+ *
+ *  designed to run directly on esp8266-01 module, to where it can be uploaded using this marvelous piece of software:
+ *
+ *  https://github.com/esp8266/Arduino
+ *
+ *  2015 Tisham Dhar
+ *  licensed under GNU GPL
+ */
+
+/*
+ *  
+ *  nodo giroscale
+ *  IP 192.168.1.89
+ *  power_01_online
+ *  MAC   60:01:94:0E:A5:24
+ *  
+ */
+
+/*
+ * File password in libraries/Secret/Secret.h
+ */
+ 
+#include <Secret.h>
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <ESP8266Influxdb.h>
+
+/******************* influxdb  *********************/
+const char *INFLUXDB_HOST = S_INFLUXDB_HOST;
+const uint16_t INFLUXDB_PORT = S_INFLUXDB_PORT;
+const char *DATABASE = S_INFLUXDB_DATABASE;
+const char *DB_USER = S_INFLUXDB_DB_USER;
+const char *DB_PASSWORD = S_INFLUXDB_DB_PASSWORD;
+
+Influxdb influxdb(INFLUXDB_HOST, INFLUXDB_PORT);
+
+/******************* OTA lib *********************/
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+/******************* OTA lib *********************/
+
+//WIFI credentials go here
+const char* wifi_ssid     = S_WIFI_SSID;
+const char* wifi_password = S_WIFI_PASSWORD;
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+//Maximum value of ADS
+#define ADC_COUNTS 32768
+#define PHASECAL 1.7
+#define VCAL 0.37
+#define ICAL 0.0086
+
+//#define VCAL 0.6
+//#define ICAL 0.003
+
+
+//#define power_topic "sensor/realpower"
+String name_client_id="ESP8266-"; //This text is concatenated with ChipId to get unique client_id
+
+//char* mode_topic = "sensor/mode";
+
+
+WiFiClient espClient;
+
+double filteredI;
+double lastFilteredV,filteredV; //Filtered_ is the raw analog value minus the DC offset
+int sampleV;                 //sample_ holds the raw analog read value
+int sampleI;
+
+double offsetV;                          //Low-pass filter output
+double offsetI;                          //Low-pass filter output
+
+double realPower,
+       apparentPower,
+       powerFactor,
+       Vrms,
+       Irms;
+double phaseShiftedV; //Holds the calibrated phase shifted voltage.
+int startV; //Instantaneous voltage at start of sample window.
+double sqV,sumV,sqI,sumI,instP,sumP; //sq = squared, sum = Sum, inst = instantaneous
+boolean lastVCross, checkVCross; //Used to measure number of times threshold is crossed.
+
+double squareRoot(double fg)
+{
+  double n = fg / 2.0;
+  double lstX = 0.0;
+  while (n != lstX)
+  {
+    lstX = n;
+    n = (n + fg / n) / 2.0;
+  }
+  return n;
+}
+
+void calcVI(unsigned int crossings, unsigned int timeout)
+{
+
+  unsigned int crossCount = 0;                             //Used to measure number of times threshold is crossed.
+  unsigned int numberOfSamples = 0;                        //This is now incremented
+
+  //-------------------------------------------------------------------------------------------------------------------------
+  // 1) Waits for the waveform to be close to 'zero' (mid-scale adc) part in sin curve.
+  //-------------------------------------------------------------------------------------------------------------------------
+  boolean st=false;                                  //an indicator to exit the while loop
+
+  unsigned long start = millis();    //millis()-start makes sure it doesnt get stuck in the loop if there is an error.
+
+  while(st==false)                                   //the while loop...
+  {
+     startV = ads.readADC_Differential_2_3();                    //using the voltage waveform
+     if ((abs(startV) < (ADC_COUNTS*0.55)) && (abs(startV) > (ADC_COUNTS*0.45))) st=true;  //check its within range
+     if ((millis()-start)>timeout) st = true;
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------------
+  // 2) Main measurement loop
+  //-------------------------------------------------------------------------------------------------------------------------
+  start = millis();
+
+  while ((crossCount < crossings) && ((millis()-start)<timeout))
+  {
+    numberOfSamples++;                       //Count number of times looped.
+    lastFilteredV = filteredV;               //Used for delay/phase compensation
+
+    //-----------------------------------------------------------------------------
+    // A) Read in raw voltage and current samples
+    //-----------------------------------------------------------------------------
+    sampleV = ads.readADC_Differential_2_3();                 //Read in raw voltage signal
+    sampleI = ads.readADC_Differential_0_1();                 //Read in raw current signal
+
+    //-----------------------------------------------------------------------------
+    // B) Apply digital low pass filters to extract the 2.5 V or 1.65 V dc offset,
+    //     then subtract this - signal is now centred on 0 counts.
+    //-----------------------------------------------------------------------------
+    offsetV = offsetV + ((sampleV-offsetV)/1024);
+  filteredV = sampleV - offsetV;
+    offsetI = offsetI + ((sampleI-offsetI)/1024);
+  filteredI = sampleI - offsetI;
+
+    //-----------------------------------------------------------------------------
+    // C) Root-mean-square method voltage
+    //-----------------------------------------------------------------------------
+    sqV= filteredV * filteredV;                 //1) square voltage values
+    sumV += sqV;                                //2) sum
+
+    //-----------------------------------------------------------------------------
+    // D) Root-mean-square method current
+    //-----------------------------------------------------------------------------
+    sqI = filteredI * filteredI;                //1) square current values
+    sumI += sqI;                                //2) sum
+
+    //-----------------------------------------------------------------------------
+    // E) Phase calibration
+    //-----------------------------------------------------------------------------
+    phaseShiftedV = lastFilteredV + PHASECAL * (filteredV - lastFilteredV);
+
+    //-----------------------------------------------------------------------------
+    // F) Instantaneous power calc
+    //-----------------------------------------------------------------------------
+    instP = phaseShiftedV * filteredI;          //Instantaneous Power
+    sumP +=instP;                               //Sum
+
+    //-----------------------------------------------------------------------------
+    // G) Find the number of times the voltage has crossed the initial voltage
+    //    - every 2 crosses we will have sampled 1 wavelength
+    //    - so this method allows us to sample an integer number of half wavelengths which increases accuracy
+    //-----------------------------------------------------------------------------
+    lastVCross = checkVCross;
+    if (sampleV > startV) checkVCross = true;
+                     else checkVCross = false;
+    if (numberOfSamples==1) lastVCross = checkVCross;
+
+    if (lastVCross != checkVCross) crossCount++;
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------------
+  // 3) Post loop calculations
+  //-------------------------------------------------------------------------------------------------------------------------
+  //Calculation of the root of the mean of the voltage and current squared (rms)
+  //Calibration coefficients applied.
+  float multiplier = 0.125F; /* ADS1115 @ +/- 4.096V gain (16-bit results) */
+  double V_RATIO = VCAL * multiplier;
+  Vrms = V_RATIO * squareRoot(sumV / numberOfSamples);
+
+  double I_RATIO = ICAL * multiplier;
+  Irms = I_RATIO * squareRoot(sumI / numberOfSamples);
+
+  //Calculation power values
+  realPower = V_RATIO * I_RATIO * sumP / numberOfSamples;
+  apparentPower = Vrms * Irms;
+  powerFactor=realPower / apparentPower;
+
+  //Reset accumulators
+  sumV = 0;
+  sumI = 0;
+  sumP = 0;
+//--------------------------------------------------------------------------------------
+}
+
+double calcIrms(unsigned int Number_of_Samples)
+{
+  /* Be sure to update this value based on the IC and the gain settings! */
+  float multiplier = 0.125F;    /* ADS1115 @ +/- 4.096V gain (16-bit results) */
+  for (unsigned int n = 0; n < Number_of_Samples; n++)
+  {
+    sampleI = ads.readADC_Differential_0_1();
+
+    // Digital low pass filter extracts the 2.5 V or 1.65 V dc offset,
+  //  then subtract this - signal is now centered on 0 counts.
+    offsetI = (offsetI + (sampleI-offsetI)/1024);
+    filteredI = sampleI - offsetI;
+    //filteredI = sampleI * multiplier;
+
+    // Root-mean-square method current
+    // 1) square current values
+    sqI = filteredI * filteredI;
+    // 2) sum
+    sumI += sqI;
+  }
+
+  Irms = squareRoot(sumI / Number_of_Samples)*multiplier;
+
+  //Reset accumulators
+  sumI = 0;
+//--------------------------------------------------------------------------------------
+
+  return Irms;
+}
+
+void setup() {
+
+ // delay(500);
+ // ESP.eraseConfig();
+ // delay(500);
+  
+  Serial.begin(115200);
+  setup_wifi();
+
+  Serial.print("Configuring OTA device...");
+  //TelnetServer.begin();   //Necesary to make Arduino Software autodetect OTA device  
+  ArduinoOTA.onStart([]() {Serial.println("OTA starting...");});
+  ArduinoOTA.onEnd([]() {Serial.println("OTA update finished!");Serial.println("Rebooting...");});
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {Serial.printf("OTA in progress: %u%%\r\n", (progress / (total / 100)));});  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OK");
+  
+  
+  
+  Serial.println("Configuring influx server...");
+  name_client_id=name_client_id+ESP.getChipId();
+  Serial.println("   Cliend Id: "+name_client_id); 
+ 
+    
+  influxdb.opendb(DATABASE, DB_USER, DB_PASSWORD);
+
+  ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+  //ads.setGain(GAIN_TWO);
+  ads.begin();
+  
+  Serial.println("Setup completed! Running ...");
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+
+long lastMsg = 0;
+float diffp = 0.5;
+//float diffp = 0;
+
+float realPowerStatic = 0.0;
+
+
+
+void loop() {
+  int16_t results;
+  ArduinoOTA.handle();
+  
+//  long now = millis();
+//  if (now - lastMsg > 2000) {
+//    lastMsg = now;
+
+  
+  /* Be sure to update this value based on the IC and the gain settings! */
+  //float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
+  float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+
+ // results = ads.readADC_Differential_0_1();  
+ // Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
+
+ // results = ads.readADC_Differential_2_3();  
+ // Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
+
+
+//  Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(trans_volt); Serial.println("mV)");
+//  double current = calcIrms(2048);
+//  Serial.print("Just Current:");
+//  Serial.println(current);
+
+  calcVI(20,2000);
+  Serial.print("Real Power:");  Serial.print(String(realPower).c_str());Serial.print(" Irms:"); Serial.print(Irms);Serial.print(" Vrms:");Serial.println(Vrms);
+  float NewrealPower = realPower;
+  
+  if (checkBound(NewrealPower,realPowerStatic , diffp)) {
+    realPowerStatic = NewrealPower;
+//    client.publish(power_topic, String(realPowerStatic).c_str(), true);
+      // Writing data using FIELD object
+        // Create field object with measurment name=power_read
+     FIELD dataObj("power_table");
+     dataObj.addTag("method", "Field_object"); // Add method tag
+     dataObj.addTag("power", "P0"); // Add pin tag
+     dataObj.addField("value", realPowerStatic); // Add value field
+     Serial.println(influxdb.write(dataObj) == DB_SUCCESS ? "Object write success" : "Writing failed");
+
+     // Empty field object.
+     dataObj.empty();
+  
+  }
+
+    delay(4000);
+
+}
